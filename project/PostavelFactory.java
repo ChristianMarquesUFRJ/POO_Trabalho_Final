7607c51e90539406c2366ca1f8e0b0e8a89b7002
Christian Marques <42151785+ChristianMarquesUFRJ@users.noreply.github.com>
2023-06-29 19:54:13 -0300

    Inicio da PostavelFactory
    getPostavel

--- project/PostavelFactory.java
+++ project/PostavelFactory.java
@@ -9,5 +9,18 @@
 
 package project;
 public class PostavelFactory {
-    
+    public static Postavel getPostavel(String tipo) throws Exception{
+        Postavel postagem;
+        if(tipo.equals("POSTVIDEO")) {
+            PostVideo postagem_video = new PostVideo();
+            postagem = postagem_video;
+        }
+        else if(tipo.equals("POSTFOTO")) {
+            PostFoto postagem_foto = new PostFoto();
+            postagem = postagem_foto;
+        }
+        else
+            throw new Exception("Este tipo de postagem ('"+ tipo +"') nao existe!");
+        return postagem;
+    }
 }
